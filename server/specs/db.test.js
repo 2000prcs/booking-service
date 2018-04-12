0bd270b0c4f087e5515e46187244c6ed27c9e500
Moyeong Lee <2000prcs@gmail.com>
2018-04-12 16:27:07 -0700

    Write unit tests for server
    - Finished unit testing for server GET / POST request

--- server/specs/db.test.js
+++ server/specs/db.test.js
@@ -1,22 +1,22 @@
 const db = require('../../database/index.js');
 
 describe('DB', () => {
-  test("should store new booking dates to 'booked_dates' array", () => {
+  test('Should store a new booking date to the room user has selected', () => {
     let testData = {
-      id: 9,
-      booked: '2018-04-12',
-      guest_name: 'Mo',
+      id: 21,
+      booked: '2018-02-20',
+      guest_name: 'Eric',
     };
 
     db.update(testData);
 
     db.findOne(testData.id, (data) => {
       let room = data;
-      expect(room.booked_dates[room.booked_dates.length - 1]).toBe('2018-04-12');
+      expect(room.booked_dates[room.booked_dates.length - 1]).toBe('2018-02-20');
     });
   });
 
-  test('should find all room data', () => {
+  test('Should find all rooms data', () => {
     db.find((data) => {
       let rooms = data;
       expect(rooms.length).toBe(100);
--- server/specs/server.test.js
+++ server/specs/server.test.js
@@ -1,33 +1,40 @@
 const request = require('request');
-const url = 'http://127.0.0.1:7777';
+const serverUrl = 'http://127.0.0.1:7777';
 
 
 describe('Server', () => {
-  test('should fetch all room data from DB by GET request', () => {
+
+
+  test('Should be able to handle GET request', () => {
     
-    request.get('/booking', (req, res) => {
-      db.find((data) => {
-        expect(data.length).toBe(100);
-      });
+    request({
+      url: `${serverUrl}/booking`,
+      method: 'GET',
+    }, (error, response, body)=>{
+      expect(response.statusCode).toBe(200);
     });
   });
 
-  test('should insert a new booking date to DB by POST request', () => {
-    server.post('/booking', (req, res) => {
-
-      let testData = {
-        id: 21,
-        booked: '2018-09-03', // add each booked dates to the booked dates array
-        guest_name: 'Mo',
-        // guest_number: 1, // sum number of adult + children
-      };
-
-      db.update(testData);
-
-      db.findOne(testData.id, (data) => {
-        let room = data;
-        expect(room.booked_dates[room.booked_dates.length - 1]).toBe('2018-09-03');
-      });
+  test('Should be able to handle POST request', () => {
+    
+    request({
+      url: `${serverUrl}/booking`,
+      method: 'POST',
+    }, (error, response, body) => {
+      expect(response.statusCode).toBe(200);
     });
+
   });
+
+  test('Should 404 when asked for a nonexistent file', ()=> {
+
+    request({
+      url: `${serverUrl}/fantasyhub`,
+      method: 'GET',
+    }, (error, response, body)=>{
+      expect(response.statusCode).toBe(404);
+    });
+
+  });
+
 });
