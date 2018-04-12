7980d467679268aa2b334308c373d46e6719363e
Moyeong Lee <2000prcs@gmail.com>
2018-04-11 19:10:02 -0700

    Basic set up for server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
 // connect express
 const express = require('express');
+
 const app = express();
 
+const port = 7777;
+
 const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+// serve client files
+app.use(express.static(path.join(__dirname, '/../client')));
 
 // import DB
+const db = require('../database');
 
 // GET request
+app.get('/booking', (req, res) => {
+  res.send('Respond to GET request: ');
+});
 
 // POST request
+app.post('/booking', (req, res) => {
+  res.send('Respond to POST reuqest: ');
+});
+
+// listen to the port
+app.listen(port, () => console.log(`Server is listening to port ${port}`));
